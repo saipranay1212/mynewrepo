75a39ffb883083393804bd278880e71de6597ffd
saipranay1212 <madugulasaipranaygoud1999@gmail.com>
2025-11-06 13:34:05 +0530

    dev2 Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,22 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+// dev-2 code added here
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+
+        input.close();
+    }
+}
